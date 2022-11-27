cc3f8ed3457acee4ceeb3e0b61fec0f4f50de197
Areen-Rath <70901420+Areen-Rath@users.noreply.github.com>
2022-11-27 22:34:19 +0530

    Update Button.js

--- js/Button.js
+++ js/Button.js
@@ -2,7 +2,7 @@ AFRAME.registerComponent("button", {
     init: function() {
         var button1 = document.createElement("button");
         button1.innerHTML = "Order Summary";
-        button1.setAttribute("id", "rating");
+        button1.setAttribute("id", "summary");
         button1.setAttribute("class", "btn btn-danger ml-3 mr-3")
         var button2 = document.createElement("button");
         button2.innerHTML = "Order Now";
